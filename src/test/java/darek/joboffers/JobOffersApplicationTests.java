9775829b9feda95d66a359a64b99a8a7ce380e38
Quang Du Vu (Darek) <vuquang.dd@gmail.com>
2024-03-12 20:05:46 +0100

    initial commit

---
+++ src/test/java/darek/joboffers/JobOffersApplicationTests.java
@@ -0,0 +1,13 @@
+package darek.joboffers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOffersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
